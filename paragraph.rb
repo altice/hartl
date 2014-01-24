c6d47c39df96b9ce12ae08a3ee82bf85255807aa
MakerSquare Student <students@themakersquare.com>
2014-01-24 13:41:40 -0600

    add chapter5 exercises

---
+++ paragraph.rb
@@ -0,0 +1,14 @@
+#Coding tests irritate a minority of people. One applicant said his resume should speak for itself. I have no time for that attitude—though I’m happy for the weeding help. Good programmers should welcome the opportunity to demonstrate they’re good at what they do. Heck, most programmers I know enjoy brain teasers anyway. They test themselves when no one’s looking.
+
+def parse text
+  array = text.split(' ')
+  hash = Hash.new
+
+  array.each do |x|
+   
+    #10 words with 1 letter
+
+  end
+end
+
+parse('All Good Boys Deserve Fudge')
