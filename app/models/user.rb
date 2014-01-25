99de38f4f2cd50f2aef588c0e55e0986d2f99024
MakerSquare Student <students@themakersquare.com>
2014-01-25 15:59:30 -0600

    Make a basic User model (including secure passwords) exit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  before_save{ self.email = email.downcase} # this makes the email lowercase prior to saving
+  validates :name, presence: true, length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX}, 
+                    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, length: {minimum: 6 }
+  
+  # has_secure_password
+ 
+  # too_long: "%{count} characters is the maximum allowed" } maximum characters allowed
+end
